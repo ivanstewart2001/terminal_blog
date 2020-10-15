def3746b04d6a3c4f21ec46d1e06a0ed5e5de8f1
ivanstewart2001 <71293769+ivanstewart2001@users.noreply.github.com>
2020-10-14 20:07:45 -0400

    Add files via upload

---
+++ terminal_blog.py
@@ -0,0 +1,12 @@
+from database import Database
+from menu import Menu
+
+Database.initialize()
+
+menu = Menu()
+
+menu.run_menu()
+
+#open terminal to start mongodb and leave open
+#before running this code
+
